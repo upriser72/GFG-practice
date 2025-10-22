1fcb5502451ab4a4c974199eaaad5723cdfa6562
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2025-10-22 12:02:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -2,8 +2,10 @@ class Solution {
   public:
     int towerOfHanoi(int n, int from, int to, int aux) {
         // code here
-        if(n<2)
-            return 1;
+        //if(n<2)
+           // return 1;
+        if(n==0)
+            return 0;
         return towerOfHanoi(n-1,from,aux,to)+1+towerOfHanoi(n-1,aux,from,to);
     }
 };
